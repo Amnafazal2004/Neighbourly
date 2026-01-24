b1f0bf85c544830b35811e39a5cf5b00574b0d3f
Amna <123323721+Amnafazal2004@users.noreply.github.com>
2026-01-24 15:30:06 +0500

    Delete ubit-connect directory

--- ubit-connect/prisma/migrations/20260109211344/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Profile" ADD COLUMN     "batch" TEXT,
-ADD COLUMN     "bio" TEXT,
-ADD COLUMN     "department" TEXT,
-ADD COLUMN     "fullName" TEXT,
-ADD COLUMN     "profilePic" TEXT,
-ADD COLUMN     "username" TEXT,
-ALTER COLUMN "seatNo" DROP NOT NULL;
